15653c4924df9e25b5ff0bbfd7cc18f6abef0a4c
云风 <cloudwu@gmail.com>
2012-08-07 13:53:03 +0800

    We don't need hashmap at all

--- Makefile
+++ Makefile
@@ -1,6 +1,5 @@
 all:
-	gcc -g -o mread -Wall mread.c ringbuffer.c map.c main.c
+	gcc -g -o mread -Wall mread.c ringbuffer.c main.c
 
 test:
-	gcc -g -o testmap -Wall map.c testmap.c
 	gcc -g -o testrb -Wall ringbuffer.c testringbuffer.c
--- mread.h
+++ mread.h
@@ -9,8 +9,8 @@ void mread_close(struct mread_pool *m);
 int mread_poll(struct mread_pool *m , int timeout);
 void * mread_pull(struct mread_pool *m , int size);
 void mread_yield(struct mread_pool *m);
-void mread_close_client(struct mread_pool *m, int id);
 int mread_closed(struct mread_pool *m);
+void mread_close_client(struct mread_pool *m, int id);
 int mread_socket(struct mread_pool *m , int index);
 
 #endif
